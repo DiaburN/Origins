641f1b8a857dc9fd961f0314b0c567227298c2e7
DiaburN <78312112+DiaburN@users.noreply.github.com>
2026-08-14 22:56:37 +0200

    Export movement animation resolver

--- packages/game-core/src/character-movement/index.ts
+++ packages/game-core/src/character-movement/index.ts
@@ -2,3 +2,4 @@ export * from "./types";
 export * from "./directions";
 export * from "./crystal-animation-profile";
 export * from "./CharacterMovementController";
+export * from "./resolveMovementAnimation";
