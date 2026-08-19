d258586704507700289666d2aac17f7d579a44f5
DiaburN <78312112+DiaburN@users.noreply.github.com>
2026-08-19 12:03:26 +0200

    feat(magic): add Crystal FrostCrunch handler

---
+++ overrides/zircon/ServerLibrary/Models/Magics/Wizard/FrostCrunch.cs
@@ -0,0 +1,15 @@
+﻿using Library;
+using Server.DBModels;
+
+namespace Server.Models.Magics
+{
+    [MagicType(MagicType.FrostCrunch)]
+    public class FrostCrunch : FireBall
+    {
+        protected override Element Element => Element.Ice;
+
+        public FrostCrunch(PlayerObject player, UserMagic magic) : base(player, magic)
+        {
+        }
+    }
+}
