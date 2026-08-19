b58f80604cfc45dddd8a1df8b6cca043174b6813
DiaburN <78312112+DiaburN@users.noreply.github.com>
2026-08-19 20:23:45 +0200

    Port Crystal Assassin Hemorrhage proc runtime

---
+++ overrides/zircon/ServerLibrary/Models/Magics/Assassin/CrystalHemorrhage.cs
@@ -0,0 +1,63 @@
+using Library;
+using Server.DBModels;
+using Server.Envir;
+using System;
+
+namespace Server.Models.Magics
+{
+    [MagicType(MagicType.CrystalHemorrhage)]
+    public sealed class CrystalHemorrhage : MagicObject
+    {
+        protected override Element Element => Element.None;
+        public override bool AttackSkill => true;
+
+        private int counter;
+        private bool ready;
+
+        public CrystalHemorrhage(PlayerObject player, UserMagic magic) : base(player, magic) { }
+
+        public override AttackCast AttackCast(MagicType attackType)
+        {
+            var response = new AttackCast();
+
+            if (ready)
+            {
+                response.Magics.Add(Type);
+                return response;
+            }
+
+            counter += SEnvir.Random.Next(1, 2 + Magic.Level * 2);
+
+            // Crystal crossing 55 arms the bleed for the next attack.
+            if (counter >= 55)
+                ready = true;
+
+            return response;
+        }
+
+        public override int ModifyPowerAdditionner(bool primary, int power, MapObject ob, Stats stats = null, int extra = 0)
+        {
+            return power + Magic.GetPower();
+        }
+
+        public override void AttackComplete(MapObject target)
+        {
+            if (target?.Node != null && !target.Dead)
+            {
+                int duration = Math.Max(1, Magic.Level * 2 + Player.Stats[Stat.Luck] / 6);
+                target.ApplyPoison(new Poison
+                {
+                    Owner = Player,
+                    Type = PoisonType.Hemorrhage,
+                    TickCount = duration,
+                    TickFrequency = TimeSpan.FromSeconds(1),
+                    Value = Player.Stats[Stat.MaxDC] + 1,
+                });
+            }
+
+            counter = 0;
+            ready = false;
+            Player.LevelMagic(Magic);
+        }
+    }
+}
