0222fa23ce0a7889833b155f48d5d01868449e98
DiaburN <78312112+DiaburN@users.noreply.github.com>
2026-08-19 16:19:48 +0200

    port Crystal FlameDisruptor runtime

---
+++ overrides/zircon/ServerLibrary/Models/Magics/Wizard/FlameDisruptor.cs
@@ -0,0 +1,67 @@
+using Library;
+using Server.DBModels;
+using Server.Envir;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Server.Models.Magics
+{
+    [MagicType(MagicType.FlameDisruptor)]
+    public sealed class FlameDisruptor : MagicObject
+    {
+        protected override Element Element => Element.Fire;
+
+        public FlameDisruptor(PlayerObject player, UserMagic magic) : base(player, magic)
+        {
+        }
+
+        public override MagicCast MagicCast(MapObject target, Point location, MirDirection direction)
+        {
+            var response = new MagicCast { Ob = target };
+
+            if (target == null || (target.Race != ObjectType.Player && target.Race != ObjectType.Monster) || !Player.CanAttackTarget(target))
+            {
+                response.Ob = null;
+                return response;
+            }
+
+            response.Targets.Add(target.ObjectID);
+
+            Point targetLocation = target.CurrentLocation;
+            int castPower = Magic.GetPower() + Player.GetMC();
+
+            bool undead = target is MonsterObject monster && monster.MonsterInfo.Undead;
+            if (!undead)
+                castPower = (int)(castPower * 1.5F);
+
+            ActionList.Add(new DelayedAction(
+                SEnvir.Now.AddMilliseconds(500),
+                ActionType.DelayMagic,
+                Type,
+                target,
+                targetLocation,
+                castPower));
+
+            return response;
+        }
+
+        public override void MagicComplete(params object[] data)
+        {
+            MapObject target = (MapObject)data[1];
+            Point targetLocation = (Point)data[2];
+            int castPower = (int)data[3];
+
+            if (target?.Node == null || target.CurrentMap != CurrentMap) return;
+            if (!Player.CanAttackTarget(target)) return;
+            if (!Functions.InRange(target.CurrentLocation, targetLocation, 2)) return;
+
+            Player.MagicAttack(new List<MagicType> { Type }, target, true, null, castPower);
+        }
+
+        public override int ModifyPowerAdditionner(bool primary, int power, MapObject ob, Stats stats = null, int extra = 0)
+        {
+            power += extra != 0 ? extra : Magic.GetPower() + Player.GetMC();
+            return power;
+        }
+    }
+}
